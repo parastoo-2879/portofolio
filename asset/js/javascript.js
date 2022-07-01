8c049ab4d4fecac73799a777ac3e785bb695b729
zahra28 <parastooghanbarzade@yahoo.com>
2022-07-02 02:39:26 +0430

    personal-page added

---
+++ asset/js/javascript.js
@@ -0,0 +1,86 @@
+document
+  .getElementsByClassName("click")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("click")[0].style.opacity = "0";
+  });
+
+document
+  .getElementsByClassName("click")[0]
+  .addEventListener("click", function () {
+    document.getElementById("menu").style.left = "0";
+  });
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
+    document.getElementById("menu").style.left = "-100%";
+  });
+
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("click")[0].style.opacity = "1";
+  });
+document
+  .getElementsByClassName("about")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-about")[0].style.top = "0";
+  });
+document
+  .getElementsByClassName("about")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-about")[0].style.transform =
+      "scale(1)";
+  });
+
+document
+  .getElementsByClassName("close1")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-about")[0].style.top = "-200%";
+  });
+
+document
+  .getElementsByClassName("skills")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-skills")[0].style.top = "0";
+  });
+
+document
+  .getElementsByClassName("close2")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-skills")[0].style.top = "-200%";
+  });
+document
+  .getElementsByClassName("education")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-education")[0].style.top = "0";
+  });
+document
+  .getElementsByClassName("close3")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-education")[0].style.top = "-200%";
+  });
+document
+  .getElementsByClassName("projects")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-project")[0].style.top = "0";
+  });
+document
+  .getElementsByClassName("close4")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-project")[0].style.top = "-200%";
+  });
+document
+  .getElementsByClassName("contact1")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-contact")[0].style.top = "0";
+  });
+document
+  .getElementsByClassName("contact")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-contact")[0].style.top = "0";
+  });
+document
+  .getElementsByClassName("close5")[0]
+  .addEventListener("click", function () {
+    document.getElementsByClassName("box-contact")[0].style.top = "-200%";
+  });
